2d68af290064ce8ed38ec449a7e6b4e4dd52580e
Ibidapo Olalekan <ibidapoazeez@gmail.com>
2020-02-12 16:00:24 +0000

    product list updated

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,5 +1,48 @@
 <template>
-    <h1>Book Form</h1>
+
+<div class="container-fluid">
+    <nav class="nav justify-content-end navbar navar-dark bg-dark">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <ul class="navbar navbar-dark bg-dark" >
+  <li class="nav-item" >
+    <a class="nav-link active" href="#">Products</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="book">Contact Us</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Login</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">call: 0800000000000</a>
+  </li>
+  <nav class="navbar navbar-dark bg-dark">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</nav>
+</ul>    
+</nav>  
+<div class="container" >
+  <div class="row-center">
+    
+    <div class="col-sm-middle">
+  <form>
+  <div class="form-group">
+    <label for="formGroupExampleInput">First Name</label>
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="First Name ">
+  </div>
+  <div class="form-group">
+    <label for="formGroupExampleInput2">Last Name</label>
+    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Last Name">
+  </div>
+</form>
+    </div>
+    
+  </div>
+</div>
+</div>
 </template>
 
 <script>
@@ -7,3 +50,8 @@
         name: 'BookForm',
     }
 </script>
+<style>
+.container{
+    color: blue;
+}
+</style>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,11 +1,74 @@
 <template>
-    <!-- <div> -->
-        <h1>Product List Table</h1>
-        <!-- <p> please</p> -->
-    <!-- </div> -->
+ <div class="container mt-5" id="move">
+    <h1>Product Page</h1>
+    <div class="table-responsive" >
+      <table class="table table-striped " border="0.25">
+        <thead class="tables">
+          <tr class="bg-dark text-white">
+            <th scope="col"><font color="white"> Serial Number:</font></th>
+            <th scope="col"><font color="white"> Name</font></th>
+            <th scope="col"><font color="white"> Price</font></th>
+            <th scope="col"><font color="white"> Plan</font></th>
+            <th scope="col"><font color="white"> Images</font></th>
+            <th scope="col"><font color="white">Action</font></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="table3">
+            <th  scope="row" class="row1">1</th>
+            <td>Starter</td>
+            <td>5000</td>
+            <td>Deluxe</td>
+            <td>Lite</td>
+            <td>Available</td>
+          </tr>
+          <tr class="table4">
+            <th scope="row" class="row2">2</th>
+            <td>Granduer</td>
+            <td>10000</td>
+            <td>Golden</td>
+            <td>Compact</td>
+            <td>Available</td>
+          </tr>
+          <tr class="table2">
+            <th scope="row" class="row3">3</th>
+            <td>Executive</td>
+            <td>15000</td>
+            <td>Luxury</td>
+            <td>Flexible</td>
+            <td class="bg-danger text-white" data-toggle="tooltip" data-placement="left"
+              title="Over-Subscribed">Booked</td>
+          </tr>
+          <tr class="table5">
+            <th scope="row" class="row4">4</th>
+            <td >Presidential</td>
+            <td>20000</td>
+            <td>Splendour</td>
+            <td>Ideal</td>
+            <td>Limited Space</td>
+          </tr>
+          <tr class="table1">
+            <th scope="row" class="row5">5</th>
+            
+            <td>Royalty</td>
+            <td>25000</td>
+            <td>Flamboyant</td>
+            <td>Serene</td>
+            <td>Limited Space</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
 </template>
 <script>
     export default{
         name: 'ProductList',        
     }
+    
 </script>
+
+<style src="./ProductList.css" scoped>
+
+</style>
