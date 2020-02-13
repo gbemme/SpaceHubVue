b0f93097f0b62fa98c78125d27a2d24109f61d5f
Ibidapo Olalekan <ibidapoazeez@gmail.com>
2020-02-13 15:19:05 +0000

    ProductForm and BookForm updated

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -19,29 +19,51 @@
   <nav class="navbar navbar-dark bg-dark">
   <form class="form-inline">
     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    <button class="btn2 btn-outline-success my-2 my-sm-0" type="submit">Search</button>
   </form>
 </nav>
 </ul>    
-</nav>  
-<div class="container" >
-  <div class="row-center">
-    
-    <div class="col-sm-middle">
-  <form>
-  <div class="form-group">
-    <label for="formGroupExampleInput">First Name</label>
-    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="First Name ">
-  </div>
-  <div class="form-group">
-    <label for="formGroupExampleInput2">Last Name</label>
-    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Last Name">
-  </div>
-</form>
-    </div>
-    
-  </div>
+</nav> 
+<div class="container">
+    <!-- form holder -->
+    <div class="formholder">
+      <!-- title holder -->
+<div class="titleHolder">
+  <i class="far fa-user-circle"></i>
+  <h1>Sign in</h1>
+  <p>Login Using Your Username and Password.</p>
 </div>
+<form:form action="${pageContext.request.contextPath}/authenticateTheUser" method="POST" class="loginForm">
+
+  <div class="row">
+     <p>
+      User name: <input type="text" name="username" placeholder="Username" required />
+       <i class="fas fa-user"></i>
+      </p>
+     </div> 
+         
+ <div class="row">
+        <p>
+          Password: <input type="password" name="password" placeholder="Password" required />
+          <i class="fas fa-key"></i>
+          </p>
+  </div>
+<div class="row">
+  <label for="rememberme"><input type="checkbox" id="rememberMe"></label>
+  
+</div>
+<div class="row">
+     <button type="submit" class="btn1">Login</button>
+</div>
+<div class="row">
+          <a href="forgot-password.html">Forgot Password?</a>
+          <br>
+          <a href="register.html">Not registered? Click to register</a>
+        </div>
+</form:form>
+    </div>      
+</div>
+
 </div>
 </template>
 
@@ -50,8 +72,8 @@
         name: 'BookForm',
     }
 </script>
-<style>
-.container{
+<style src="./bookform.css" scoped>
+.container {
     color: blue;
 }
 </style>
